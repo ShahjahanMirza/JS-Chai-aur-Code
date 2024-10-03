eb7e524788c6c91a1014997e08caaa3b44de6adc
Shahjahan Mirza <103453568+ShahjahanMirza@users.noreply.github.com>
2024-10-03 07:53:34 +0000

    commit -m "done"

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict"; // treat all JS code as newer version, but no need to mention it 
+
+
+console.log(typeof null);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+// let score = 33
+// console.log(typeof score)
+// console.log();
+
+// score = "33"
+// console.log(typeof (score))
+// console.log();
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log();
+
+// score = "33abc"
+// valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber);
+// console.log();
+
+// score = NaN
+// valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber);
+// console.log();
+
+// score = undefined
+// valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+/*
+1 => true; 0 => false
+"" => false
+"abc" => true
+*/
+
+// *********************** OPERATIONS *************************
+
+
+
